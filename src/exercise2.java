409f8d7ab5fc9080c07f58e934c127c3e6e29a0a
Linda Amaya <linda.amaya.cetina@gmail.com>
2021-08-23 22:42:56 -0500

    exercise 2 is complete

---
+++ src/exercise2.java
@@ -0,0 +1,17 @@
+public class exercise2 {
+    public static void main(String[] args) {
+        //Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los dos.
+        //Si son iguales indicarlo también. Ves cambiando los valores para comprobar que funciona.
+
+        int a = 3;
+        int b = 6;
+        if(a > b){
+            System.out.println(a + " es mayor que " + b);
+        }else if( a == b){
+            System.out.println(a + " y " + b + " son iguales.");
+        }else{
+            System.out.println(a + " es menor que " + b);
+        }
+
+    }
+}
