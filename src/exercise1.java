9ff70c1d5e437adb148f32e3d19c8178c8e66380
Linda Amaya <linda.amaya.cetina@gmail.com>
2021-08-23 22:35:07 -0500

    exercise 1 is developed

--- src/exercise1.java
+++ src/exercise1.java
@@ -1,5 +1,9 @@
 public class exercise1 {
     public static void main(String[] args) {
+    //Declara dos variables numéricas (con el valor que desees), muestra por consola la suma, resta, multiplicación, división y módulo (resto de la división).
 
+        int a = 6;
+        int b = 4;
+        System.out.println("Suma " + (a+b) + "\nResta " + (a-b) + "\nMultiplicación " + (a*b) + "\nDivisión " + (a/b) + "\nMódulo " + (a%b) );
     }
 }
