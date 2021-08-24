11ad61bf3c06234b7c654ad0195ee3e6245e3fb0
Linda Amaya <linda.amaya.cetina@gmail.com>
2021-08-23 22:47:48 -0500

    exercise 3 is successful

---
+++ src/exercise3.java
@@ -0,0 +1,9 @@
+public class exercise3 {
+    public static void main(String[] args) {
+        //Declara un String que contenga tu nombre, después muestra un mensaje de bienvenida por consola.
+        //Por ejemplo: si introduzco “Fernando”, me aparezca “Bienvenido Fernando”.
+
+        String nombre = "Julieth";
+        System.out.println("Bienvenido " + nombre);
+    }
+}
